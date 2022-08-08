133d71f7c496728690a488b0441547cfb38f9360
Carlos Lisandra <carlos.lisandra@toolfactory.net>
2022-08-08 16:45:41 +0200

    server reflection & change addr of server

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
 use tonic::{transport::Server, Request, Response, Status};
-
+use std::net::{IpAddr, Ipv4Addr, SocketAddr};
 use bookstore::bookstore_server::{Bookstore, BookstoreServer};
 use bookstore::{GetBookRequest, GetBookResponse};
 
 
 mod bookstore {
     include!("bookstore.rs");
+
+    // Add this
+    pub(crate) const FILE_DESCRIPTOR_SET: &[u8] =
+        tonic::include_file_descriptor_set!("greeter_descriptor");
 }
 
 
@@ -31,13 +35,20 @@ impl Bookstore for BookStoreImpl {
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let addr = "[::1]:50051".parse().unwrap();
+    let addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 5001);
     let bookstore = BookStoreImpl::default();
+
+    // Add this
+    let reflection_service = tonic_reflection::server::Builder::configure()
+        .register_encoded_file_descriptor_set(bookstore::FILE_DESCRIPTOR_SET)
+        .build()
+        .unwrap();
 
     println!("Bookstore server listening on {}", addr);
 
     Server::builder()
         .add_service(BookstoreServer::new(bookstore))
+        .add_service(reflection_service) // Add this
         .serve(addr)
         .await?;
 
